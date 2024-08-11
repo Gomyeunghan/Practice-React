167783ea600de982ad87eba17290eb5757482134
Gomyeunghan <rhaudgks04@gmail.com>
2024-08-12 01:01:49 +0900

    10.히스토리 포기.. 내일 마저하겠습니다..

--- Tic-Tac-Toe__begin/src/Game/Game.jsx
+++ Tic-Tac-Toe__begin/src/Game/Game.jsx
@@ -1,17 +1,12 @@
 import Board from '../components/Board/Board';
+import History from '../components/History/History';
 import './Game.css';
 
 export default function Game() {
   return (
     <div className="Game">
       <Board></Board>
-      <div className="History">
-        <ol>
-          <li>
-            <button type="button">게임 시작</button>
-          </li>
-        </ol>
-      </div>
+      <History></History>
     </div>
   );
 }
