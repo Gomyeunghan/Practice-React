3e527764e3181bb2f35f512c59abc73fc3620644
Gomyeunghan <rhaudgks04@gmail.com>
2024-08-11 15:23:46 +0900

    5.버튼에 children prop이 들어오면, disabled 에 children prop 을 전달해준다.
    -children prop은 null or nextplayer 이기 때문에, 플레이어 값이 들어오면 disabled 속성을 가지도록 구성함

--- Tic-Tac-Toe__begin/src/components/Square/Square.jsx
+++ Tic-Tac-Toe__begin/src/components/Square/Square.jsx
@@ -1,6 +1,11 @@
 export default function Square({ squareClick, children }) {
   return (
-    <button type="button" className="Square" onClick={squareClick}>
+    <button
+      type="button"
+      className="Square"
+      onClick={squareClick}
+      disabled={children}
+    >
       {children}
     </button>
   );
